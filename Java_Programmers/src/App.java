9c7e96b989cd18b3dbe2e51c8ccabe3e27e56375
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-10-12 20:28:18 +0900

    Level_1 짝수와 홀수

--- Java_Programmers/src/App.java
+++ Java_Programmers/src/App.java
@@ -1,5 +1,8 @@
+import Level_1.짝수와홀수.Solution;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Solution solution = new Solution();
+        System.out.println(solution.solution(4));
     }
 }
---
+++ Java_Programmers/src/Level_1/짝수와홀수/Solution.java
@@ -0,0 +1,13 @@
+package Level_1.짝수와홀수;
+
+public class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if(num % 2 == 0) {
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
