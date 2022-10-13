712f89e783d18aba3fae713ff178f770ed77445a
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-10-14 00:14:55 +0900

    run project

--- Java_Programmers/src/App.java
+++ Java_Programmers/src/App.java
@@ -3,6 +3,6 @@ import Level_1.자릿수더하기.Solution;
 public class App {
     public static void main(String[] args) throws Exception {
         Solution solution = new Solution();
-        System.out.println(solution.solution(4));
+        System.out.println(solution.solution(987));
     }
 }
