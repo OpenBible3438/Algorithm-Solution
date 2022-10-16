803304fb60b991abd8a3d45db797ef1f28681284
openbible <59015538+OpenBible3438@users.noreply.github.com>
2022-10-16 19:09:58 +0900

    Code Test

--- Java_Programmers/src/App.java
+++ Java_Programmers/src/App.java
@@ -1,8 +1,8 @@
-import Level_1.자릿수더하기.Solution;
+import Level_2.최댓값과최솟값.Solution;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Solution solution = new Solution();
-        System.out.println(solution.solution(987));
+        System.out.println(solution.solution("-1 -2 -3 -5"));
     }
 }
