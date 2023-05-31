fe64a3b590ff8f4146ca229e0d06fa1baab0d375
openbible <59015538+OpenBible3438@users.noreply.github.com>
2023-05-31 15:06:24 +0900

    Update README.md

--- Java_Programmers/src/App.java
+++
@@ -1,8 +0,0 @@
-import Level_2.최댓값과최솟값.Solution;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Solution solution = new Solution();
-        System.out.println(solution.solution("-1 -2 -3 -5"));
-    }
-}
