60c83dcacdd5153eab2247840a689a8d288982aa
openbible <59015538+OpenBible3438@users.noreply.github.com>
2023-06-22 11:13:41 +0900

    배열 조각하기

---
+++ Swift_Programmers/Swift_Programmers/Lv_0/배열 조각하기/배열_조각하기.swift
@@ -0,0 +1,27 @@
+//
+//  배열_조각하기.swift
+//  Swift_Programmers
+//
+//  Created by 편성경 on 2023/06/22.
+//
+
+import Foundation
+
+func solution_배열_조각하기(_ arr:[Int], _ query:[Int]) -> [Int] {
+    
+    var result = arr
+    
+    for i in 0 ..< query.count {
+        if i % 2 == 0{
+            // 짝수 인덱스 -> 인덱스 기준으로 뒷부분 버리고 앞부분 얻기
+            let index = query[i]
+            result = Array(result.prefix(upTo: index + 1))
+        } else {
+            // 홀수 인덱스 -> 인덱스 기준으로 앞부분 버리고 뒷부분 얻기
+            let index = query[i]
+            result = Array(result.suffix(from: index))
+        }
+    }
+    
+    return result
+}
